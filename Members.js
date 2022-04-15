b8700e4489001d5de061d689ec1cbf1307991110
wteo <wendyteo.wy@gmail.com>
2022-04-15 16:44:27 +1000

    Notes over how to initialixe a middleware function

---
+++ Members.js
@@ -0,0 +1,18 @@
+const members = [{
+    id      : 1,
+    name    : "John Smith",
+    email   : "johnsmith@gmail.com",
+    status  : "active"
+}, {
+    id      : 2,
+    name    : "Kevin White",
+    email   : "kevinwhite@hotmail.com",
+    status  : "active"
+}, {
+    id      : 3, 
+    name    : "Amy Wong",
+    email   : "amywong@gmail.com",
+    status  : "inactive"
+}]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const path = require("path") // node.JS method
+const moment = require("moment");
+const members = require("./Members");
+
 
 const app = express();
 
-/*
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html")); 
-    // This allows you to upload file. Though. not the most ideal approach as you will need to manually put a separate route each time you upload a file.
-    // In particular, if only intend to create a static website using only HTML and CSS.
-})
-*/
+const logger = (req, res, next) => {
+    // You can write whatever code you want here.
+    console.log(`${req.protocol}://${req.get('host')}${
+        req.originalUrl}: ${moment().format()}`);
+    next();
+    
+}
+
+// To initialize your middleware
+app.use(logger);
+
+// Gets All Members
+app.get("/api/members", (req, res) => {
+    res.json(members);
+    // this returns an object as a json file;
+}); 
 
 // Set static folder
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public"))); // this is the more ideal way to publish a static page.
 // "".use()"" is a method used when want to include a middleware
+// You can add as many html files as you want in the public folder.
+// But this isn't what you mostly use express for.
 
 const PORT = process.env.PORT || 5000;
 
